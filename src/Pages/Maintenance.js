685654c3bd7c2cfb6cf5b8d056cea85d4b8714f4
Joao <jg.cristovao@campus.fct.unl.pt>
2024-01-13 22:51:09 +0000

    Updated Maintenance.js

--- src/Pages/Maintenance.js
+++ src/Pages/Maintenance.js
@@ -1,4 +1,4 @@
-import { Box, ImageListItem, Typography, styled } from '@mui/material';
+import { Box, ImageListItem, Typography, styled, ThemeProvider } from '@mui/material';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import EmailIcon from '@mui/icons-material/Email';
 import React from 'react';
@@ -7,21 +7,22 @@ import Logo from '../images/logos/logos-03.png';
 import noiseImage from '../images/other/Noise.png';
 import top1Image from '../images/other/In.Nova_III JE Portugal-224.jpg';
 import top2Image from '../images/other/top2.jpg';
-import bottom1Image from '../images/other/bottom1.jpg';
+import bottom1Image from '../images/other/IMG_0908 (1).jpg';
 import bottom2Image from '../images/other/bottom2.jpg';
 import bottom3Image from '../images/other/bottom3.jpg';
 
+import useMediaQuery from '@mui/material/useMediaQuery';
+
 
 const Maintenance = () => {
+  const isScreenHeightLessThan400px = useMediaQuery('(max-height: 400px)');
   return (
       <Box
       sx={{
         width: "100vw",
         height: "100vh",
         backgroundColor: "#052533",
-        display: {
-          lg: "flex"
-        },
+        display: "flex",
         alignItems: "center",
         justifyContent: {
           xs: "center",
@@ -33,297 +34,268 @@ const Maintenance = () => {
       }}
     >
 
+<Box
+  sx={{
+    position: "absolute",
+    top: "0%",
+    left: "0%",
+    width: "95%",
+    height: "100vh",
+    overflow: "hidden",
+    display: {
+      xs: "none",
+      lg: "block",
+    },
+  }}
+>
 
-<Box sx={{
-  position: "absolute",
-  left: "0px",
-  height: "100vh",
-  overflow: "hidden",
-  display:{
-    xs: "none",
-    lg: "block"
-  }
-}}>
- {/*Elementos de cima, da esquerda para a direita */}
+  {/* Barras de cima, da esquerda para a direita */}
 
- <div style={{
-  width: "auto",
-  position: "relative",
-  top: "53%",
-  left: "-39%",
-  transform: "rotate(60deg)"
-}}>
-  <div style={{
-    width: "70%",
-    height: "8vw",
-    backgroundColor: "#732043",
-    flexShrink: "0",    
-    borderRadius: "0 10vw 10vw 0",
+<div id='topBar1' style={{
     position: "relative",
-    overflow: "hidden"
+    top: "20%",
+    left: "-47%",
+    transform: "rotate(60deg)",
   }}>
     <div style={{
+      width: "90vw",
+      height: "8vw",
       backgroundColor: "#732043",
-      opacity: "0.85",
-    }}>
-      <img src={top1Image} style={{
-        opacity: "0.7",  
-        width: "100%",
-        height: "auto",
-        borderRadius: "0 10vw 10vw 0",
-        objectFit: "cover",
-        transform: "rotate(-60deg) translateY(10%) translateX(45%) scale(1.65)",
-      }} />
-    </div>
-  </div>
-</div>
-
-<div style={{
-    position: "relative",
-    top: "8%",
-    left: "-23%",
-    transform: "rotate(60deg)"
-}}>
-<div style={{
-  width: "65%",
-  height: "8vw",
-  backgroundColor: "#732043",
-  flexShrink: "0",    
-  borderRadius: "0 10vw 10vw 0",
-  position: "relative",
-  overflow: "hidden"
-}}>
-  <div style={{
-    backgroundColor: "#732043",
-    opacity: "0.85",
-  }}>
-    <img src={top2Image} style={{
-      opacity: "0.7",  
-      width: "100%",
-      height: "auto",
+      flexShrink: "0",
       borderRadius: "0 10vw 10vw 0",
-      objectFit: "cover",
-      transform: "rotate(-60deg) translateY(-15%) translateX(40%) scale(1.45)",
-    }} />
-  </div>
-</div>
-</div>
-
-{/*Elementos de baixo, da esquerda para a direita */}
-
-<div id='bottomBar1' style={{
-    position: "relative",
-    bottom: "25%",
-    left: "-62%",
-    transform: "rotate(-120deg)"
-  }}>
-  <div style={{
-    width: "20%",
-    height: "8vw",
-    backgroundColor: "#732043",
-    flexShrink: "0",    
-    borderRadius: "0 10vw 10vw 0",
-    position: "relative",
-    overflow: "hidden"
-  }}>
-    <div style={{
-      backgroundColor: "#732043",
-      opacity: "0.85",
+      overflow: "hidden"
     }}>
-      <img src={bottom1Image} style={{
-        opacity: "0.7",  
-        width: "100%",
-        height: "auto",
-        borderRadius: "0 10vw 10vw 0",
-        objectFit: "cover",
-        transform: "rotate(120deg) translateY(0%) translateX(15%) scale(1.25)",
-      }} />
+      <div style={{
+        backgroundColor: "#732043",
+        opacity: "0.85",
+      }}>
+        <img src={top1Image} style={{
+          opacity: "0.7",
+          width: "100%",
+          height: "auto",
+          borderRadius: "0 10vw 10vw 0",
+          objectFit: "cover",
+          transform: "rotate(-60deg) translateY(10%) translateX(45%) scale(1.65)",
+        }} />
+      </div>
     </div>
   </div>
-</div>
 
-<div id='bottomBar2' style={{
+  <div id='topBar2' style={{
     position: "relative",
-    bottom: "10%",
-    left: "-30%",
-    transform: "rotate(-120deg)"
-  }}>
-  <div style={{
-    width: "47%",
-    height: "8vw",
-    backgroundColor: "#732043",
-    flexShrink: "0",    
-    borderRadius: "0 10vw 10vw 0",
-    position: "relative",
-    overflow: "hidden"
+    top: "-5%",
+    left: "-25%",
+    transform: "rotate(60deg)",
   }}>
     <div style={{
+      width: "80%",
+      height: "8vw",
       backgroundColor: "#732043",
-      opacity: "0.85",
+      flexShrink: "0",
+      borderRadius: "0 10vw 10vw 0",
+      overflow: "hidden"
     }}>
-      <img src={bottom2Image} style={{
-        opacity: "0.7",  
-        width: "100%",
-        height: "auto",
-        borderRadius: "0 10vw 10vw 0",
-        objectFit: "cover",
-        transform: "rotate(120deg) translateY(0%) translateX(-70%) scale(1)",
-      }} />
+      <div style={{
+        backgroundColor: "#732043",
+        opacity: "0.85",
+      }}>
+        <img src={top2Image} style={{
+          opacity: "0.7",
+          width: "100%",
+          height: "auto",
+          borderRadius: "0 10vw 10vw 0",
+          objectFit: "cover",
+          transform: "rotate(-60deg) translateY(-5%) translateX(40%) scale(1)",
+        }} />
+      </div>
     </div>
   </div>
-</div>
+  
 
-<div id='bottomBar3' style={{
+  {/* Barras de baixo, da esquerda para a direita */}
+
+  <div id='bottomBar1' style={{
     position: "relative",
-    bottom: "40%",
-    left: "-10%",
-    transform: "rotate(-120deg)"
-  }}>
-  <div style={{
-    width: "50%",
-    height: "8vw",
-    backgroundColor: "#732043",
-    flexShrink: "0",    
-    borderRadius: "0 10vw 10vw 0",
-    position: "relative",
-    overflow: "hidden"        
+    bottom: "-30%",
+    left: "-47%",
+    transform: "rotate(-120deg)",
   }}>
     <div style={{
+      width: "50%",
+      height: "8vw",
       backgroundColor: "#732043",
-      opacity: "0.85",
+      flexShrink: "0",
+      borderRadius: "0 10vw 10vw 0",
+      overflow: "hidden"
     }}>
-      <img src={bottom3Image} style={{
-        opacity: "0.7",  
-        width: "100%",
-        height: "auto",
-        borderRadius: "0 10vw 10vw 0",
-        objectFit: "cover",
-        transform: "rotate(120deg) translateY(15%) translateX(-40%) scale(1.8)",
-      }} />
+      <div style={{
+        backgroundColor: "#732043",
+        opacity: "0.85",
+      }}>
+        <img src={bottom1Image} style={{
+          opacity: "0.7",
+          width: "100%",
+          height: "auto",
+          borderRadius: "0 10vw 10vw 0",
+          objectFit: "cover",
+          transform: "rotate(120deg) translateY(-11%) translateX(-67%) scale(1)",
+        }} />
+      </div>
     </div>
   </div>
-</div>
+
+  <div id='bottomBar2' style={{
+    position: "relative",
+    bottom: "-18%",
+    left: "-21%",
+    transform: "rotate(-120deg)"
+  }}>
+    <div style={{
+      width: "60vw",
+      height: "8vw",
+      backgroundColor: "#732043",
+      flexShrink: "0",
+      borderRadius: "0 10vw 10vw 0",
+      overflow: "hidden"
+    }}>
+      <div style={{
+        backgroundColor: "#732043",
+        opacity: "0.85",
+      }}>
+        <img src={bottom2Image} style={{
+          opacity: "0.7",
+          width: "100%",
+          height: "auto",
+          borderRadius: "0 10vw 10vw 0",
+          objectFit: "cover",
+          transform: "rotate(120deg) translateY(-10%) translateX(-68%) scale(1)",
+        }} />
+      </div>
+    </div>
+  </div>
+
+  <div id='bottomBar3' style={{
+    position: "relative",
+    bottom: "0%",
+    left: "3%",
+    transform: "rotate(-120deg)",
+  }}>
+    <div style={{
+      width: "75%",
+      height: "8vw",
+      backgroundColor: "#732043",
+      flexShrink: "0",
+      borderRadius: "0 10vw 10vw 0",
+      overflow: "hidden"
+    }}>
+      <div style={{
+        backgroundColor: "#732043",
+        opacity: "0.85",
+      }}>
+        <img src={bottom3Image} style={{
+          opacity: "0.7",
+          width: "100%",
+          height: "auto",
+          borderRadius: "0 10vw 10vw 0",
+          objectFit: "cover",
+          transform: "rotate(120deg) translateY(5%) translateX(-42%) scale(1.2)",
+        }} />
+      </div>
+    </div>
+  </div>
 </Box>
 
 
-<Box sx={{//Div para a versão mobile
+<Box sx={{//Versão mobile
   display: {
     xs: "flex",
     lg:"none"
   },
-  width: "100%",
+  flexDirection: "column",
+  width: "100vw",
+  height: "100vh",
   position: "absolute",
-  top: "15%",       // Vertically center
-  left: "50%",      // Horizontally center
-  transform: "translate(-50%, -50%)",  // Center both horizontally and vertically
   overflow: "hidden"
 }}>
-<Typography variant='h3' color="white" sx={{
-        textAlign: "center",
-        marginTop: "50%",
-        fontSize: "60px",
-        color: '#D2D2D2',  
-        fontFamily: 'Exo 2', 
-        fontWeight: '600', 
-        wordWrap: 'break-word'
-    }}>
+
+<Typography
+  variant='h3'
+  color="white"
+  sx={{
+    textAlign: "center",
+    fontSize: "65px",
+    color: '#D2D2D2',  
+    fontWeight: '600', 
+    wordWrap: 'break-word',
+    position: "absolute",
+    top: "20%",
+    width: '100%',
+    zIndex:"2"
+  }}>
         Oops!
     </Typography>
+    
     <Typography variant='h2' color="white" sx={{
+        position: "absolute",
+        top: "37%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
         textAlign: "center",
-        margin: "5% auto",
-        fontSize: "30px",
+        fontSize: "35px",
+        fontFamily: 'Exo 2, sans-serif',
         color: '#D2D2D2',  
-        fontFamily: 'Exo 2', 
-        fontWeight: '400', 
+        fontWeight: '500', 
         wordWrap: 'break-word',
-        width: "80%"
+        width: "90%",
+        zIndex: "2"
     }}>
         This website is currently under construction!
     </Typography>
 
-    <ImageListItem sx={{
-  
-    }}>
-      <img src={Logo} alt="Logo" style={{ width: "100%", height: "auto"}} />
-    </ImageListItem>
-
-</Box>
-
-<Box sx={{//Div para a versão Desktop
-  display: {
-    xs: "none",
-    lg:"flex"
-  },
-  justifyContent: "right",
-  position: "relative",
-  height: "100vh",
-  width: "50%",
-  right: "5%"
-}}>
-
-<Typography variant='h3' color="white" sx={{
-        display:{
-          xs: "none",
-          lg: "flex"
-        },
-        width:'642px',
-        height: "216px",
-        textAlign: 'right', 
-        margin: "35% 0%",
-        color: '#D2D2D2', 
-        fontSize: "60px", 
-        fontFamily: 'Exo 2', 
-        fontWeight: '600', 
-        wordWrap: 'break-word'
-    }}>
-        Oops! This website is currently under construction!
-    </Typography>
-    
-  <ImageListItem sx={{
-    width: "89px",
+    <ImageListItem
+  sx={{
     position: "absolute",
-    height: "100vh",
-    overflow: "hidden",
-  }}>
-  <img src={Logo} alt="Logo" style={{ width: "100%", height: "auto"}} />
+    bottom: "-23vh",
+    right: "-15vw",
+    zIndex: "1"
+  }}
+>
+  <img
+    src={Logo}
+    alt="Logo"
+    style={{
+      maxWidth: "600px",
+      width: "145vw",
+      height: "auto",
+    }}
+  />
 </ImageListItem>
 
 <Box
   sx={{
     position: "absolute",
-    bottom: "20%",
-    display: "flex",
+    bottom: "42%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    display: isScreenHeightLessThan400px ? 'none' : 'flex', //Nao esta a funcionar
     flexDirection: "row",
     gap: "15px",
-    alignItems: "center",
-    zIndex: "999",
+    zIndex: "2"
   }}
 >
   <InstagramIcon
     sx={{
       color: "white",
-      width: {
-        xs: "33px",
-        lg:"43px"
-      },
-      height: "auto"
+      width: "27px",
+      height: "auto",
     }}
   />
   <Typography
     color="white"
     sx={{
-      height:{
-        xs: "33px",
-        lg: "43px"
-      },
       color: 'white',
-      fontSize: {
-        xs: "22px",
-        lg: "24px"
-    },
-      fontFamily: 'Exo 2',
-      fontWeight: '400',
+      fontSize: "21px",
+      fontWeight: '200',
       wordWrap: 'break-word',
       display: 'flex',
       alignItems: 'center',
@@ -336,23 +308,130 @@ const Maintenance = () => {
 <Box
   sx={{
     position: "absolute",
-    bottom: {
-      xs: "45%",
-      lg: "68px",
-    },
-    left: {
-      xs: "20.5%", // Nao pode ficar assim
-      lg: "80vw",
-    },
-    width: "auto",
+    bottom: "37%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
     display: "flex",
     flexDirection: "row",
     gap: "15px",
-    alignItems: "center",
-    height:{
-      xs: "33px",
-      lg: "43px"
-    }
+    zIndex: "2"
+  }}
+>
+  <EmailIcon
+    sx={{
+      color: "white",
+      width: "27px",
+      height: "auto",
+    }}
+  />
+  <Typography
+    color="white"
+    sx={{
+      color: 'white',
+      fontSize: "21px",
+      fontWeight: '200',
+      wordWrap: 'break-word',
+      alignItems: 'center',
+    }}
+  >
+    geral@innova.pt
+  </Typography>
+</Box>
+
+
+
+</Box>
+
+
+<Box sx={{//Versão Desktop
+  display: {
+    xs: "none",
+    lg:"flex"
+  },
+  justifyContent: "right",
+  position: "relative",
+  height: "100vh",
+  width: "50%",
+  margin: "0 5% 0 0"
+}}>
+
+<Typography variant='h3' color="white" sx={{
+        display:{
+          xs: "none",
+          lg: "flex"
+        },
+        width:'42vw',
+        height: "22vh",
+        position: "absolute",
+        top: "35%",
+        textAlign: 'right', 
+        color: '#D2D2D2', 
+        fontSize: "60px", 
+        fontWeight: '600', 
+        wordWrap: 'break-word'
+    }}>
+        Oops! This website is currently under construction!
+    </Typography>
+    
+  <ImageListItem sx={{
+    width: "10%",
+    position: "absolute",
+    overflow: "hidden",
+    margin: "8% -1.5% 0 0"
+  }}>
+  <img src={Logo} alt="Logo" style={{ width: "100%", height: "auto"}} />
+</ImageListItem>
+
+
+<Box
+  sx={{
+    position: "absolute",
+    bottom: "14%",
+    display: "flex",
+    flexDirection: "row",
+    gap: "15px",
+    zIndex: "999",
+  }}
+>
+  <InstagramIcon
+    sx={{
+      color: "white",
+      width: {
+        xs: "33px",
+        lg: "43px",
+      },
+      height: "auto",
+    }}
+  />
+  <Typography
+    color="white"
+    sx={{
+      height: {
+        xs: "33px",
+        lg: "43px",
+      },
+      color: 'white',
+      fontSize: {
+        xs: "22px",
+        lg: "24px",
+      },
+      fontWeight: '200',
+      wordWrap: 'break-word'
+    }}
+  >
+    @in.nova.pt
+  </Typography>
+</Box>
+
+
+<Box
+  sx={{
+    position: "absolute",
+    bottom: "20%",
+    display: "flex",
+    flexDirection: "row",
+    gap: "15px",
+    zIndex: "999",
   }}
 >
   <EmailIcon
@@ -377,21 +456,17 @@ const Maintenance = () => {
         xs: "22px",
         lg: "24px"
     },
-      fontFamily: 'Exo 2',
-      fontWeight: '400',
+      fontWeight: '200',
       wordWrap: 'break-word',
-      alignItems: 'center',
     }}>
     geral@innova.pt
   </Typography>
 </Box>
+</Box>
 
 </Box>
 
 
-
-</Box>
-    
   );
 };
 
--- src/theme.js
+++ src/theme.js
@@ -1,7 +1,6 @@
 import { createTheme } from "@mui/material";
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Exo+2" />
 
-
 export const theme = createTheme({
     typography: {
         fontFamily: 'Exo 2, sans-serif',
