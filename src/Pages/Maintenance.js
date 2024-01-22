4dadd9bf431efa3126b6a2e9721109c549e79f7f
Trinca <t.trinca@campus.fct.unl.pt>
2024-01-22 18:06:12 +0000

    Fix image paths and import App component

--- src/Pages/Maintenance.js
+++ src/Pages/Maintenance.js
@@ -4,11 +4,11 @@ import EmailIcon from '@mui/icons-material/Email';
 import React from 'react';
 import Logo from '../public/images/logos/logos-03.png';
 import noiseImage from '../public/images/other/Noise_1.webp';
-import top1Image from '/images/other/top1.webp';
-import top2Image from '../../public/images/other/top2.webp';
-import bottom1Image from '../../public/images/other/bottom1.webp';
-import bottom2Image from '../../public/images/other/bottom2.webp';
-import bottom3Image from '../../public/images/other/bottom3.webp';
+import top1Image from '../public/images/other/top1.webp';
+import top2Image from '../public/images/other/top2.webp';
+import bottom1Image from '../public/images/other/bottom1.webp';
+import bottom2Image from '../public/images/other/bottom2.webp';
+import bottom3Image from '../public/images/other/bottom3.webp';
 
 import { useMediaQuery } from 'react-responsive';
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { ThemeProvider } from '@mui/material';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-//import App from './App';
+import App from './App';
 import {theme} from './theme';
 import { BrowserRouter } from 'react-router-dom';
 import Maintenance from './Pages/Maintenance';
