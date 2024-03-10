f9c618d9d9f4b8c19ec0cabe1a46bea4fb7b04f2
Joao <jg.cristovao@campus.fct.unl.pt>
2024-03-10 01:16:09 +0000

    Update BgMenu
    Fixed BgMenu closing bug and changed the animation

--- src/components/BgMenu.jsx
+++ src/components/BgMenu.jsx
@@ -1,15 +1,19 @@
 import React , {useState} from 'react';
-import { NavLink, useNavigate } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { Typography } from '@mui/material';
-
+import { useMediaQuery } from 'react-responsive';
 
 const BgMenu = () => {
+
+  const mobile = useMediaQuery({ maxWidth: 600 });
+  const tablet = useMediaQuery({minWidth: 601, maxWidth: 1080});
+  const desktop = useMediaQuery({ minWidth: 1081 });
 
   const [burger_class, setBurgerClass] = useState("burger-bar unclicked")
   const [menu_class, setMenuClass] = useState("menu hidden")
   const [isMenuClicked, setIsMenuClicked] = useState(false)
 
-  const updateMenu = () => {
+  const updateMenu = (state) => {
     if (!isMenuClicked) {
       document.body.style.overflow = 'hidden';
       setBurgerClass("burger-bar clicked");
@@ -19,11 +23,12 @@ const BgMenu = () => {
         height: "400dvh",
         width: "100dvw",
         backgroundColor: "#732043",
-        right: 0,
+        top: 0,
+        right: 0,//efeito a vir da direita
         marginRight: 0,
-        borderRadius: "25%",
+        borderRadius: "0%",
         zIndex: 50,
-        transition: "right 0.4s ease-in-out, marginRight 0.4s ease-in-out, height 0.4s ease-in-out, width 0.4s ease-in-out, border-radius 0.4s ease-in-out, backgroundColor 0.4s ease-in-out",
+        transition: "right 0.4s ease-in-out, marginRight 0.4s ease-in-out, height 0.5s ease-in-out, width 0.4s ease-in-out, border-radius 0.4s ease-in-out, backgroundColor 0.4s ease-in-out",
       });
     } else {
       document.body.style.overflow = 'auto';
@@ -31,11 +36,13 @@ const BgMenu = () => {
       setMenuClass("menu hidden");
       setMenuButtonStyle({
         ...MenuButtonStyle,
-        height: "0px",
-        width: "0px",
+        height: "100dvh",        
+        width: "0px",//faz o efeito a vir da direita
+        //height: "0px",
+        //width: "100dvw" faz o efeito a vir de cima
         position: "absolute",
-        right: "10%",
-        borderRadius: "50%",
+        right: "0%",
+        borderRadius: "0%",
         backgroundColor: "#732043"
       });
       setBgMenuStyle({
@@ -49,16 +56,16 @@ const BgMenu = () => {
   
   const [MenuButtonStyle, setMenuButtonStyle] = useState({
     color: 'white',
-    height: "0px",
+    height: "100dvh",
     width: "0px",
     aspectRatio: "1/1",
     backgroundColor: "transparent",
-    borderRadius: "50%",
     boxShadow: "none",
     border: "none",
     overflow: "hidden",
     position: "absolute",
-    right: "7%",
+    top: 0,
+    right: 0
   });
 
   const [BgMenuStyle, setBgMenuStyle] = useState({
@@ -67,7 +74,6 @@ const BgMenu = () => {
     width: "40px",
     aspectRatio: "1/1",
     backgroundColor: "transparent",
-    borderRadius: "50%",
     boxShadow: "none",
     border: "none",
     display: "flex",
@@ -86,7 +92,7 @@ const BgMenu = () => {
     height: "3px",
     borderRadius: "5px",
     backgroundColor: isMenuClicked ? "white" : "#732043",
-    transform: isMenuClicked ? "rotate(45deg) translate(0em, 0.7em)" : "none",
+    transform: isMenuClicked ? "rotate(45deg) translate(0.2em, 0.5em)" : "none",
     transition: "transform ease-in-out 0.5s, backgroundColor ease-in-out 0.5s",
   };
   
@@ -104,7 +110,7 @@ const BgMenu = () => {
     height: "3px",
     borderRadius: "5px",
     backgroundColor: isMenuClicked ? "white" : "#732043",   
-    transform: isMenuClicked ? "rotate(-45deg) translate(0em, -0.7em)" : "none",
+    transform: isMenuClicked ? "rotate(-45deg) translate(0.2em, -0.5em)" : "none",
     transition: "transform ease-in-out 0.5s, backgroundColor ease-in-out 0.5s",
   }
 
@@ -118,12 +124,11 @@ const BgMenu = () => {
     fontWeight: "400"
   };
 
-  const navigate = useNavigate();
-
-  const handleNavLinkClick = (path) => {
-    navigate.push(path);
-    window.scrollTo(0, 0);
+  const handleNavLinkClick = () => {
+    //updateMenu();
+    setIsMenuClicked(false)   
   };
+
 
   return (
     <div style={{ width: "100%", height: "100%", display: "flex", alignItems: "center", overflow: "hidden"}}>
@@ -150,24 +155,29 @@ const BgMenu = () => {
     bottom: 0
   }}>
     
-      <NavLink to="/about-us" style={NavLinkStyle}>
+      <NavLink to="/about-us" style={NavLinkStyle} onClick={updateMenu}>
           <Typography style={PagesLinkStyle}>About Us</Typography>
       </NavLink> 
-      <NavLink to="/services" style={NavLinkStyle} >
+      <NavLink to="/services" style={NavLinkStyle} onClick={updateMenu}>
           <Typography style={PagesLinkStyle}>Services</Typography>
       </NavLink>
-      <NavLink to="/our-people/our-team" style={NavLinkStyle} >
+      <NavLink to="/our-people/our-team" style={NavLinkStyle} onClick={updateMenu}>
           <Typography style={PagesLinkStyle}>Our Team</Typography>
       </NavLink>
-      <NavLink to="/our-people/our-advisors" style={NavLinkStyle} >
+      <NavLink to="/our-people/our-advisors" style={NavLinkStyle} onClick={updateMenu}>
           <Typography style={PagesLinkStyle}>Our Advisors</Typography>
       </NavLink>
-      <NavLink to="/join-us" style={NavLinkStyle} >
+      <NavLink to="/join-us" style={NavLinkStyle} onClick={updateMenu}>
           <Typography style={PagesLinkStyle}>Events</Typography>
       </NavLink>
-      <NavLink to="/out-of-office" style={NavLinkStyle} >
+      <NavLink to="/out-of-office" style={NavLinkStyle} onClick={updateMenu}>
           <Typography style={PagesLinkStyle}>Out of Office</Typography>    
-      </NavLink>              
+      </NavLink>
+      {mobile && (
+        <NavLink to="/join-us" style={NavLinkStyle} onClick={updateMenu}>
+          <Typography style={PagesLinkStyle}>Join Us</Typography>
+        </NavLink>
+      )}            
     
   </div>
 
