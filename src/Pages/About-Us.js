af4b717aa5122bbb616da2f9c9c4e93cf98850c4
Inês Costa <imma.costa@campus.fct.unl.pt>
2024-03-13 20:50:15 +0000

    mission vision values

--- src/Pages/About-Us.js
+++ src/Pages/About-Us.js
@@ -185,21 +185,21 @@ const AboutUs = () => {
                         <Values
                             title="Mission"
                             subtitle="What impact do we <br>want to create?"
-                            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vehicula viverra leo id aliquam. Sed ullamcorper nulla nibh, id pulvinar eros ultrices maximus."
+                            content="Promote the interaction of students in the business environment, training them in the latest scientific and technological techniques that enable them to develop creative and innovative solutions applied to real problems."
                             currentState={selectedButton === 'Mission' ? 'expanded' : selectedButton ? 'collapsed' : 'normal'}
                             onButtonClick={handleButtonClick}
                         />
                         <Values
                             title="Vision"
                             subtitle="What impact do we <br>want to create?"
-                            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vehicula viverra leo id aliquam. Sed ullamcorper nulla nibh, id pulvinar eros ultrices maximus."
+                            content="In-Nova aims to involve the academic and technology communities, highlighting the quality and excellence that characterize NOVA SST and ultimately making university students more competent, entrepreneurial and critical."
                             currentState={selectedButton === 'Vision' ? 'expanded' : selectedButton ? 'collapsed' : 'normal'}
                             onButtonClick={handleButtonClick}
                         />
                         <Values
                             title="Values"
                             subtitle="What impact do we <br>want to create?"
-                            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vehicula viverra leo id aliquam. Sed ullamcorper nulla nibh, id pulvinar eros ultrices maximus."
+                            content="Empowering the entrepreneurial spirit, giving priority to creativity, prioritizing critical thinking and encouraging proactivity and teamwork."
                             currentState={selectedButton === 'Values' ? 'expanded' : selectedButton ? 'collapsed' : 'normal'}
                             onButtonClick={handleButtonClick}
                         />
